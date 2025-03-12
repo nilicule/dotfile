11d638cb6b15fdfb10d9957cdb0c7f01e0451c72
Remco Brink <nilicule@gmail.com>
2025-03-12 13:36:16 +0100

    Update

--- dot_oh-my-zsh/custom/plex.zsh
+++ dot_oh-my-zsh/custom/plex.zsh
@@ -1,10 +1,30 @@
-function plex() {
-  echo "Copying $1 to server"
-  scp -r $1 flow@rc6.org:~/.plexupload/new/
-
-  # Escape filename
-  printf -v filename "%q" "$1"
-
-  echo "Processing $filename on server"
-  ssh flow@rc6.org "cd ~/.plexupload/new && ~/bin/rtorrent-scp $filename"
+plex () {
+    local original_name="$1"
+    
+    # 1. Replace square brackets with nothing (keeping the content inside)
+    # 2. Replace parentheses with nothing
+    # 3. Trim leading/trailing whitespace
+    # 4. Replace spaces with periods
+    # 5. Remove consecutive periods
+    local safe_name=$(echo "$original_name" | 
+        sed 's/\[//g; s/\]//g' | 
+        sed 's/(//g; s/)//g' | 
+        sed 's/^[ \t]*//;s/[ \t]*$//' | 
+        sed 's/ /./g' | 
+        sed 's/\.\.*/./g')
+    
+    echo "Copying $safe_name to server ($original_name)"
+    
+    # Create a temporary directory for the renamed file
+    local temp_dir=$(mktemp -d)
+    cp -r "$original_name" "$temp_dir/$safe_name"
+    
+    # Upload the renamed file
+    scp -r "$temp_dir/$safe_name" flow@rc6.org:~/.plexupload/new/
+    
+    # Process on the server
+    ssh flow@rc6.org "cd ~/.plexupload/new && ~/bin/rtorrent-scp $safe_name"
+    
+    # Clean up
+    rm -rf "$temp_dir"
 }
